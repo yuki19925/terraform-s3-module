f3647bfc0fbb11d68957b98b4c5cb5493b427fb6
yuki19925 <107262941+yuki19925@users.noreply.github.com>
2025-12-16 20:13:03 +0900

    Fix interpolation syntax in S3 bucket resource

--- main.tf
+++ main.tf
@@ -6,7 +6,7 @@ resource "random_string" "bucket_suffix" {
 }
 
 resource "aws_s3_bucket" "this" {
-  bucket = var.bucket_name != "" ? "$$ {var.bucket_name}- $${random_string.bucket_suffix.result}" : "my-terraform-bucket-${random_string.bucket_suffix.result}"
+  bucket = var.bucket_name != "" ? "${var.bucket_name}-${random_string.bucket_suffix.result}" : "my-terraform-bucket-${random_string.bucket_suffix.result}"
 
   tags = {
     Name        = var.bucket_name != "" ? var.bucket_name : "My Terraform Bucket"
